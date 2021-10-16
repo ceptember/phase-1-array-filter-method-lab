45b815a59d7d38ca71e583b87c1fb363ae6da96b
Christy Perozzi <christy.perozzi@gmail.com>
2021-10-16 14:31:08 -0400

    commit tree 253805cca6a06d14f46c333c9af63b73b319c9e7

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(arr, str){
+    return arr.filter(item => item.toUpperCase() === str.toUpperCase())
+}
+
+
+function fuzzyMatch(arr, str){
+    return arr.filter(item => item.toUpperCase().startsWith(str.toUpperCase()) );
+};
+
+function matchName(arr, str){
+    return arr.filter( item => item.name.toUpperCase() === str.toUpperCase())
+};
